197fe4c6191f7c093cb98cf821b6583d13e881c3
Wenyu Lee <wenyulee51@gmail.com>
2013-09-26 13:14:33 +0800

    Update html, css and fonts

--- config.rb
+++ config.rb
@@ -5,6 +5,7 @@ css_dir = "css"
 sass_dir = "sass"
 images_dir = "images"
 javascripts_dir = "js"
+fonts_dir = "fonts"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
@@ -12,7 +13,6 @@ javascripts_dir = "js"
 # To enable relative paths to assets via compass helper functions. Uncomment:
 # relative_assets = true
 
-# To disable debugging comments that display the original location of your selectors. Uncomment:
 line_comments = false
 
 
